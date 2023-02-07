207d40e38dd7f0ae9513b54afe8cc4967c8ab37d
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2023-02-07 08:07:24 +0530

    dashboard done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import countries from "./countries";
+import dropdown from "./dropdown.js";
+import plus from "./images/plus.jpeg";
 
 import useStyles from "./styles.js";
 
-import {
-  faAddressBook,
-  faBars,
-  faContactBook,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const App = () => {
   const [value, setValue] = useState("");
   const [result, setresult] = useState([]);
@@ -35,49 +34,118 @@ const App = () => {
         <Grid item xs={2}>
           <div
             style={{
-              height: "10vh",
-              width: "100%",
               display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-evenly",
-              alignItems: "center",
+              flexDirection: "column",
+              gap: "20px",
+              marginTop: "15px",
             }}
           >
-            <FontAwesomeIcon
-              style={{
-                color: "#5f6368",
-                opacity: "1",
-                height: "25px",
-              }}
-              icon={faBars}
-            />
-
             <div
               style={{
+                height: "10vh",
+                width: "100%",
                 display: "flex",
                 flexDirection: "row",
-                justifyContent: "center",
+                justifyContent: "space-evenly",
                 alignItems: "center",
               }}
             >
-              <img
+              <FontAwesomeIcon
                 style={{
-                  height: "60px",
-                  lineHeight: "24px",
+                  color: "#5f6368",
+                  opacity: "1",
+                  height: "25px",
                 }}
-                src="https://play-lh.googleusercontent.com/fvhPW8dpGXM42Y-6aQU8Yl25L1l_mVgeoM-n08FxAkM7umAHkNs8wcs4MA49E67a7WVt"
+                icon={faBars}
               />
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  style={{
+                    height: "60px",
+                    lineHeight: "24px",
+                  }}
+                  src="https://play-lh.googleusercontent.com/fvhPW8dpGXM42Y-6aQU8Yl25L1l_mVgeoM-n08FxAkM7umAHkNs8wcs4MA49E67a7WVt"
+                />
+                <Typography
+                  style={{
+                    color: "#5f6368",
+                    opacity: "1",
+                    fontSize: "22px",
+                    lineHeight: "24px",
+                  }}
+                >
+                  Contacts
+                </Typography>
+              </div>
+            </div>
+
+            <Button className={classes.button} variant="contained">
+              <FontAwesomeIcon
+                style={{
+                  color: "yellow",
+                  opacity: "1",
+                  height: "25px",
+                }}
+                icon={faPlus}
+              />
+
               <Typography
                 style={{
                   color: "#5f6368",
                   opacity: "1",
-                  fontSize: "22px",
+                  fontSize: "15px",
                   lineHeight: "24px",
                 }}
               >
-                Contacts
+                Create contact
               </Typography>
-            </div>
+            </Button>
+
+            {dropdown.map((data) => (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  // justifyContent: "center",
+                  gap: "10px",
+                  lineHeight: "1.3em",
+                  marginLeft: "20px",
+
+                  opacity: ".8",
+                  overflow: "hidden",
+                  transition: ".15s transform",
+                }}
+              >
+                <FontAwesomeIcon
+                  style={{
+                    color: "#616161",
+                    opacity: "1",
+                    height: "20px",
+                  }}
+                  icon={data.img}
+                />
+
+                <div style={{ width: "10px" }}></div>
+
+                <Typography
+                  style={{
+                    color: "#616161",
+                    cursor: "pointer",
+                    height: "7px",
+                  }}
+                >
+                  {data.name}
+                </Typography>
+              </div>
+            ))}
           </div>
         </Grid>
 
---
+++ src/dropdown.js
@@ -0,0 +1,28 @@
+import {
+  faAddressBook,
+  faBars,
+  faCodeMerge,
+  faContactBook,
+  faDumpster,
+  faFileExport,
+  faFileImport,
+  faPlus,
+  faPrint,
+  faStopwatch,
+} from "@fortawesome/free-solid-svg-icons";
+import { faDropbox } from "@fortawesome/free-brands-svg-icons";
+import { faContactCard } from "@fortawesome/free-regular-svg-icons";
+const dropdown = [
+  { key: 1, name: "Contacts", img: faContactBook },
+  { key: 2, name: "Frequently contacted", img: faStopwatch },
+  { key: 3, name: "Merge and Fix", img: faCodeMerge },
+  { key: 4, name: "Label", img: faDropbox },
+  { key: 5, name: "Create Label", img: faPlus },
+  { key: 6, name: "Import", img: faFileImport },
+  { key: 7, name: "Export", img: faFileExport },
+  { key: 8, name: "Print", img: faPrint },
+  { key: 9, name: "Other Contacts", img: faContactCard },
+  { key: 10, name: "Bin", img: faDumpster },
+];
+
+export default dropdown;
